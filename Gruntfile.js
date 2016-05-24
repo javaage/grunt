1152ca27cad93516a378fc7c0a25d7ac5a5d75ce
javaage <hb_java@sina.com>
2016-05-25 07:35:50 +0800

    submit all

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,18 @@
 module.exports = function(grunt){
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
+		copy: {
+		    templates: {
+		      files: [{
+		        expand: true,
+		        cwd: "src",     // Parent folder of original CSS templates
+		        src: '**/*.*',             // Collects all `*.css` files within the parent folder (and its subfolders)
+		        dest: 'dist'            // Stores the collected `*.css` files in your `src/css/` folder
+		      }]
+		    }
+		  }
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default',[]);
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.registerTask('default',['copy']);
 }
